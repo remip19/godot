96aff8533c274418eacbac97e1f2363a50dd1fde
neuneutrinos <neuneutrinos@gmail.com>
2023-06-01 09:38:53 +0200

    Rajout singleton GLOBAL pour l'acces au constantes globales

---
+++ script/Global/Autoload_Global.gd
@@ -0,0 +1,6 @@
+#class "structure" pour rendre accessible des données globalement
+#Cette class doit être dans l'autoload pour fonctionner corrctement
+extends Object
+class_name Autoload_Global
+
+const JETON = {"V_INIT":-2,"ACCELERATION":-10}
