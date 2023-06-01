cf8c15f12e7f6c3072c70d627a5a35aa8aec7913
neuneutrinos <neuneutrinos@gmail.com>
2023-06-01 09:46:45 +0200

    Stop le process des jeton une fois immobile

--- script/jeton.gd
+++ script/jeton.gd
@@ -1,26 +1,21 @@
 extends MeshInstance3D
 class_name Jeton
 
+#gestion de la vitesse des jetons et de leur accélération à faire !
+
 var y_final: int
-var is_moving: bool = true
 
 func _init(column:int,y_start:int,_y_final):
 	transform.origin=Vector3(0,y_start,column)
 	self.y_final=_y_final
 	pass
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	transform.origin.y -= delta
 	
 	# Vérifie si le pion a atteint la dernière rangée (y_final)
 	if transform.origin.y <= y_final:
-		is_moving = false
+		set_process(false)#la fonction _process ne sera plus appelé
 		transform.origin.y = y_final
 
 #v=v+a*delta
